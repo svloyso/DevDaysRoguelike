e344f9f215b3f213ff72ab123d950b5c564fa299
tatyana <tatyana.kuzina11@gmail.com>
2015-11-04 13:50:23 +0300

    13.50

--- UI/src/drawinfo.cpp
+++ UI/src/drawinfo.cpp
@@ -3,10 +3,9 @@
 DrawInfo::DrawInfo(int _hero_max_hp, int _hero_cur_hp)
     : hero_max_hp (_hero_max_hp)
     , hero_cur_hp (_hero_cur_hp)
-    , norma (hero_cur_hp / hero_max_hp)
 {
-    
 }
+
 void DrawInfo::draw_hp()
 {
     Coord x (2, 4);
@@ -20,19 +19,36 @@ void DrawInfo::draw_hp()
 }
 void DrawInfo::draw_hero_stats (int _hero_cur_hp)
 {
+    draw_empty_hp_line ();
     hero_cur_hp = _hero_cur_hp;
     norma = hero_cur_hp / hero_max_hp;
+    compute_diff ();
     if (norma == 1)
     {
-        draw_hp();
-        draw_full_hp_line();
+        draw_hp ();
+        draw_full_hp_line ();
+        return;
+    }
+    if (norma >= 0.8)
+    {
+        draw_hp ();
+        draw_green_hp_line ();
         return;
     }
 //    if (norma >= 0.4)
 //    {
 //        draw_mid_hp();
 //        draw_mid_hp_line();
-//        return;
+//      void DrawInfo::draw_empty_hp_line ()
+    {
+            cout << "\033[6;4H" << "|";
+            cout << "    "<<  "\033[00m" << "|";
+            cout << "    "<<  "\033[00m" << "|";
+            cout << "    "<<  "\033[00m" << "|";
+            cout << "    "<<  "\033[00m" << "|";
+            cout << "    "<<  "\033[00m" << "|";
+
+    }  return;
 //    }
     // printf("%i / %i", unitsData[heroIndex].health, GetUnitDefaultHealth(UnitType_Hero));
 
@@ -55,8 +71,19 @@ void DrawInfo::draw_hero_stats (int _hero_cur_hp)
     //         printf("%c", 179);
     // }
 }
+void DrawInfo::draw_empty_hp_line ()
+{
+        cout << "\033[6;4H" << "|" <<  "\033[00m";
+        cout << "    "<<  "\033[00m" << "|";
+        cout << "    "<<  "\033[00m" << "|";
+        cout << "    "<<  "\033[00m" << "|";
+        cout << "    "<<  "\033[00m" << "|";
+        cout << "    "<<  "\033[00m" << "|";
+
+}
 void DrawInfo::draw_full_hp_line ()
 {
+
         cout << "\033[6;4H" << "|";
         cout << "\033[42m" << "    "<<  "\033[00m" << "|";
         cout << "\033[42m" << "    "<<  "\033[00m" << "|";
@@ -64,13 +91,33 @@ void DrawInfo::draw_full_hp_line ()
         cout << "\033[42m" << "    "<<  "\033[00m" << "|";
         cout << "\033[42m" << "    "<<  "\033[00m" << "|";
 }
-void draw_mid_hp_line ()
+void DrawInfo::draw_green_hp_line ()
 {
-//    cout << "\033[4;8H" << "|";
-//    cout << "\033[43m" << "    "<<  "\033[00m" << "|";
-//    cout << "\033[43m" << "    "<<  "\033[00m" << "|";
+    cout << "\033[4;8H" << "|";
+    cout << "\033[42m" << "    "<<  "\033[00m" << "|";
+    cout << "\033[42m" << "    "<<  "\033[00m" << "|";
+    cout << "\033[42m" << "    "<<  "\033[00m" << "|";
+    cout << "\033[42m" << "    "<<  "\033[00m" << "|";
+    draw_one_part(42);
+}
+void DrawInfo::draw_one_part (int color)
+{
+    cout << "\033[" << color << "m";
+    for (int i = 0; i < fractions_count; ++i)
+    {
+        cout  << " ";
+    }
+    cout << "\033[00m";
+    for (int i = 0; i < 5 - fractions_count; --i)
+    {
+        cout << " ";
+    }
+    cout << "|";
+}
 
-//    cout << "\033[42m" << "    "<<  "\033[00m" << "|";
-//    cout << "\033[42m" << "    "<<  "\033[00m" << "|";
-//    cout << "\033[42m" << "    "<<  "\033[00m" << "|";
+int DrawInfo::compute_diff ()
+{
+    one_part_hp = hero_max_hp / 5;
+    full_parts_count = hero_cur_hp / one_part_hp;
+    fractions_count = (hero_cur_hp - full_parts_count * one_part_hp ) % 5;
 }
