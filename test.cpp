8fdecc25370da51a56ef1dbc502a13b37b280ddf
svloyso <svloyso@gmail.com>
2015-11-04 15:28:41 +0300

    Сборка

---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#include "core.h"
+#include "getLevel.h"
+
+void print_map() {
+    MapInfo info = main_core->get_mapinfo();
+    int width = info.size.x;
+    int height = info.size.y;
+
+    for(int x = 0; x < width; ++x) {
+        for(int y = 0; y < height; ++y) {
+            TilePtr tile = main_core->get_tile(Coord(x, y));
+            if( tile->get_type() == TileType::Wall ) {
+                std::cout << '#';
+            } 
+            if ( tile->get_type() == TileType::Floor ) {
+                if (tile->get_immovables().size()) {
+                    std::cout << 'D';
+                } else {
+                    std::cout << '.';
+                }
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+
+int main() {
+    MapInfo info;
+    info.size = Coord();
+    getMap();
+    print_map();
+}
