c2a1232597c3f3cf83ac8a01e1854aa5d6efb039
Nikita Ivanov <ramzesxi@gmail.com>
2015-11-05 04:55:15 +0300

    this doesn't work

--- core/src/monster.cpp
+++ core/src/monster.cpp
@@ -3,7 +3,7 @@
 Monster::Monster(const AIPtr _ai, MonsterStatsPtr _stats) : Unit(_stats), ai(_ai) { }
 
 void Monster::act() {
-    ai->act(get_my_ptr());
+    //ai->act(get_my_ptr());
 }
 
 void Monster::react(ActionPtr action) {
